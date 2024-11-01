37144124d55d63c05003bc965b4196f73e1bc5d5
andreiderin <andreiderinucoz@gmail.com>
2024-10-31 19:48:21 -0500

    m

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ const riskRoutes = require('./routes/riskRoutes');
 app.use(express.json());
 app.use(cors());
 
-// MongoDB connection
-mongoose.connect('mongodb+srv://aucoz:Ucoz2002.@rabotest.doo4m.mongodb.net/?retryWrites=true&w=majority&appName=raboTest', {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
 .then(() => console.log('MongoDB connected successfully'))
 .catch(err => console.error('MongoDB connection error:', err));
+
 
 // Use the routes
 app.use('/api', riskRoutes);
